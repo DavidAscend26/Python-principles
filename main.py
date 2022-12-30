9d442ef7fd7fcb45c6496e2d98daf5d42ce6c43f
DavidAscend26 <ascenddavid@gmail.com>
2022-12-30 17:59:04 -0600

    If Else Conditional

---
+++ main.py
@@ -0,0 +1,7 @@
+# Enviar saludo a consola con Python
+resultado = int(input("Ingresa un número: "))
+
+if resultado % 2 == 0:
+    print(f'El valor de {resultado} es número par')
+else:
+    print(f'El valor de {resultado} es número impar')
